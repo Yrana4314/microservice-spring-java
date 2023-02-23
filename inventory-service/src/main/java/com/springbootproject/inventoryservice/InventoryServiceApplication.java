d521ce5672999a6c2fa37ed8d9f8187c492ac1ca
Yogesh Rana <yrana4314@gmail.com>
2023-02-23 10:38:23 -0500

    Eureka server setUp

--- inventory-service/src/main/java/com/springbootproject/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/springbootproject/inventoryservice/InventoryServiceApplication.java
@@ -5,10 +5,12 @@ import com.springbootproject.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
+
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
