cce8ff7c5a0afb2a7b5b1beaf3f60fafe04663fb
Yogesh Rana <yrana4314@gmail.com>
2023-03-03 20:11:40 -0500

    Microservices Secured with KeyCloak

---
+++ discovery-server/src/main/java/com/springbootproject/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.springbootproject.discoveryserver.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${app.eureka.username}")
+    private String username;
+    @Value("${app.eureka.password}")
+    private String password;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .authorities("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf().disable()
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return httpSecurity.build();
+    }
+    /*
+    .roles("USER")
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser(username).password(password)
+                .authorities("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+*/
+}
+
